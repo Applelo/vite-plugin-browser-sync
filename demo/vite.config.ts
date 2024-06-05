e70cd3afa0d4a74e0365ff8334e6d0b4ead6536d
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2024-06-05 22:22:44 +0200

    update dependencies and CI

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   // },
   plugins: [
     VitePluginBrowserSync({
+      dev: {
+        enable: true,
+        bs: {
+          online: true,
+        },
+      },
       preview: {
         enable: true,
         bs: {
