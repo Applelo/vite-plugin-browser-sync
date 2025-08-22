74b9437c43d83330f972c574a70f7cf798a20026
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2025-08-22 17:58:49 +0200

    update dependencies

--- demo/basic/vite.config.ts
+++ demo/basic/vite.config.ts
@@ -6,30 +6,6 @@ export default defineConfig({
   //   watch: {},
   // },
   plugins: [
-    VitePluginBrowserSync({
-      dev: {
-        enable: true,
-        bs: {
-          online: true,
-        },
-      },
-      preview: {
-        enable: true,
-        bs: {
-          proxy: 'http://localhost:3000',
-        },
-      },
-      buildWatch: {
-        enable: true,
-        // mode: 'snippet',
-        bs: {
-          // server: 'dist',
-          proxy: 'http://localhost:3000',
-          // proxy: {
-          //   target: 'http://localhost:3000',
-          // },
-        },
-      },
-    }),
+    VitePluginBrowserSync(),
   ],
 })
